aa30e11829674aaa437b8e8ccc6775fe50d9a056
Hubert <1842014@cegep-ste-foy.qc.ca>
2020-11-02 22:37:05 -0500

    Modification uml et Simulator

--- tp1/src/Simulator.java
+++ tp1/src/Simulator.java
@@ -16,29 +16,37 @@ import skills.Sword;
 
 public class Simulator {
 
+	private static final int ANY_VALUE = 20;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new Simulator();
 	}
 	Attributes warriorAttributes = new Attributes(40, 25,20, 15);
-	Skills sword = new Sword(20);
-	Skills shield = new Sheild(20);
+	Skills sword = new Sword(ANY_VALUE);
+	Skills shield = new Sheild(ANY_VALUE);
 	Attributes athleteAttributes = new Attributes(20, 20, 20, 20);
-	Skills offenseSpell = new OffenseSpell(20);
-	Skills defenseSpell = new DefenseSpell(20);
+	Skills offenseSpell = new OffenseSpell(ANY_VALUE);
+	Skills defenseSpell = new DefenseSpell(ANY_VALUE);
 	Attributes wizardAttributes = new Attributes(10, 10, 40, 40);
-	Skills healSpell = new HealSpell(20);
-	Skills remedy = new Remedy(20);
+	Skills healSpell = new HealSpell(ANY_VALUE);
+	Skills remedy = new Remedy(ANY_VALUE);
 	
 	private List<Fighter> listOfFighter = new ArrayList<Fighter>();
 	
 	public Simulator() {
 		this.seedData();
 		this.printData("Voici les combatants");
+		this.decreaseHealth();
+		this.printData("Combatants àpres réducation de la vie");
+	}
+
+	private void decreaseHealth() {
+		for (Fighter fighter : listOfFighter) {
+			fighter.decreaseHealthPoints(ANY_VALUE);
+		}
 	}
 
 	private void printData(String title) {
-		// TODO Auto-generated method stub
 		System.out.println();
         System.out.println(title);
         for (Fighter fighter: listOfFighter) {
@@ -47,7 +55,6 @@ public class Simulator {
 	}
 
 	private void seedData() {
-		// TODO Auto-generated method stub
 		this.listOfFighter.add(new Warrior("Bob", warriorAttributes, sword, shield));
 		this.listOfFighter.add(new Wizard("Merlon", wizardAttributes, offenseSpell, defenseSpell));
 		this.listOfFighter.add(new Athlete("Ryu", athleteAttributes, healSpell, remedy));
